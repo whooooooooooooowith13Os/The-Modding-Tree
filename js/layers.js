70a05f93c18a6b070858655665b7c62b3be58985
whooooooooooooowith13Os <93753106+whooooooooooooowith13Os@users.noreply.github.com>
2021-12-03 21:17:37 -0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,6 +1,6 @@
 addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    name: "test", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "T", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
@@ -9,7 +9,7 @@ addLayer("p", {
     color: "#4BDC13",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "prestige points", // Name of prestige currency
-    baseResource: "points", // Name of resource prestige is based on
+    baseResource: "test", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
